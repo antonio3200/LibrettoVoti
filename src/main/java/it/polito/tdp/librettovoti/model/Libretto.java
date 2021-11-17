13724e98f6c632047da150b82727a82bc2d8ad1a
x510u <x510u@DESKTOP-OSJ806G>
2021-11-17 09:15:11 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -13,8 +13,36 @@ public class Libretto {
 		this.voti.add(v);
 		
 	}
+	/*
+	public void StampaVotiUguali(int punteggio) {
+		//il libretto stampa solo i voti
+	}
+	public String VotiUguali(int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sara poi il chiamante a stamparli
+	}
+	*/
 	
+	public List<Voto> listaVotiUguali(int punteggio){
+		//restituisce solo i voti uguali al criterio
+		
+		List<Voto> risultato= new ArrayList<>();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio)
+				risultato.add(v);
+		}
+		return risultato;
+	}
 	
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato= new Libretto();
+		for(Voto v : this.voti) {
+			if(v.getVoto()==punteggio)
+				risultato.add(v);
+		}
+		return risultato;
+	}
+
 	public String toString() {
 		String s="";
 		for(Voto v : this.voti) {
--- src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/TestLibretto.java
@@ -1,6 +1,7 @@
 package it.polito.tdp.librettovoti.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 	
@@ -13,8 +14,15 @@ public class TestLibretto {
    libretto.add(voto1);
    libretto.add(new Voto("Fisica 1",28,LocalDate.of(2019, 7, 15)));
    libretto.add(new Voto("Chimica",24,LocalDate.of(2019, 9, 15)));
-   
+   libretto.add(new Voto("Informatica",25,LocalDate.of(2019, 9, 21)));
    
    System.out.println(libretto);
+   
+   
+   List<Voto> venticinque= libretto.listaVotiUguali(25);
+   System.out.println(venticinque);
+   
+   Libretto librettoventicinque= libretto.votiUguali(25);
+   System.out.println(librettoventicinque);
 }
 	}
